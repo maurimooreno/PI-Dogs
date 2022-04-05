2080e36fd6794048f432683fef66b7e8d5801b4f
maurimooreno <maurimooreno@gmail.com>
2022-04-05 13:38:23 -0300

    modificando archivos para deploy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
+import App from './App.jsx';
 import {Provider} from 'react-redux';
-import store from './store';
+import store from './store/index.jsx';
 
 ReactDOM.render(
   <Provider store={store}>
